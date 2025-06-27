9ccf9316850510f5e47398ea37190b8a4ae84ebf
MWMaresch <mwmaresch@gmail.com>
2025-06-27 15:14:01 -0400

    attempt at fetching data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch('/api/first-cost-by-route')
+      .then((res) => res.text())
+      .then((text) => {
+        console.log('Raw response:', text);
+        })
+      .catch((err) => console.error('Error:', err));
+  }, []);
 
   return (
     <>
