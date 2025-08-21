5a977649cd8e88ed7f32b0b7d8933885d37bd888
Ryan Perera <ryanperera9001@gmail.com>
2025-08-20 20:47:23 -0400

    D

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
+//t
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "https://RyanPerera.github.io/ShouldIRip",
